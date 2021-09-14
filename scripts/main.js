b9b964d4e417538f3378fdd4b1bc0499954599d3
coopertade <cetade@mail.fhsu.edu>
2021-09-14 17:42:28 -0500

    Week Four Assignment

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+//Q.1
+var myNumber = 0;
+
+//Q.2
+myNumber = 5;
+
+//Q.3
+var myOtherNumber = 5;
+
+//Q.4
+var myTotal = myNumber * myOtherNumber;
+
+//Q.5
+var myName = 'cooper';
+var myCombo = `${myNumber + myOtherNumber}`
+
+//Q.6
+var myOtherCombo = myNumber + myOtherNumber;
+
+//Q.7
+var myRemainder = myNumber % 5;
+
+//Q.8
+var i = 2;
+
+//Q.9
+var int = 4;
+
+//Q.10
+var myBoolean = false;
